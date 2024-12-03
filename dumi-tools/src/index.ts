8fde1cbcdd42a1a1d8b1b7136dceeb6f6f1bdc50
Yaobin.Deng <15118995517@163.com>
2024-12-03 11:54:14 +0800

    删除 dumi

--- dumi-tools/src/index.ts
+++
@@ -1,6 +0,0 @@
-// ---------------------------------- common start ---------------------------------- //
-
-export { default as consoleStyle } from './common/console-style';
-export { default as Request } from './common/request';
-
-// ---------------------------------- common  end  ---------------------------------- //
